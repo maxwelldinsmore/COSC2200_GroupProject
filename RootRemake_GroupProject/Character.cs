af723e1bf2f03eb5ce547b7e5cc214e676599bcf
Maxwell Dinsmore <maxwell.dinsmore@dcmail.ca>
2025-03-05 16:05:06 -0500

    added basic character and card class

---
+++ RootRemake_GroupProject/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootRemake_GroupProject
+{
+    internal class Character
+    {
+        public string usersName;
+        public string characterName;
+        public int TotalWarriors;
+        public int TotalBuildings;
+        public int 
+    }
+}
---
+++ RootRemake_GroupProject/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootRemake_GroupProject
+{
+    internal class Player
+    {
+        public int UserName;
+        public Character character;
+        public int VictoryPoints;
+        public Card[] hand;
+    }
+}
