8dafa38a53bba9d7b204f731f14fc7709c86353b
marroco11 <157555802+marroco11@users.noreply.github.com>
2025-03-25 13:52:29 -0400

    Vegabond

---
+++ RootRemake_Project/CharacterClasses/Vegabond.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootRemake_Project.CharacterClasses
+{
+    public class Vegabond
+    {
+
+        public string Name { get; set; }
+        public int RuinItems { get; set; }
+        public Dictionary<string, int> FactionRank { get; set; }
+        public List<string> Quests { get; set; }
+
+        public Vegabond(string name)
+        {
+            Name = name;
+            RuinItems = 0;
+            FactionRank = new Dictionary<string, int>();
+            Quests = new List<string>();
+
+        }
+
+        public void ExploreRuin()
+        {
+            RuinItems++;
+
+        }
+
+        public void ChangeReputation(string faction, int amount)
+        {
+            if (!FactionRank.ContainsKey(faction))
+                FactionRank[faction] = 0;
+
+            FactionRank[faction] += amount;
+        }
+
+        public void CompleteQuest()
+        {
+            Quests.Add(Quests[0]);
+        }
+
+
+    }
+}
